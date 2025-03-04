17962f3a5322a03fd0990dc585e5079388caffe8
PashaNotebook <p@korshunov.me>
2025-03-04 17:58:23 +0500

    show

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ function getFiles() {
 
 function processCommand(command) {
     switch (command) {
+        case 'show':
+            for (let file of getFiles()) {
+                for (let line of file.split('\n')) {
+                    let comment = line.match(/(\/\/\s*TODO.*)/);
+                    if (comment) {
+                        console.log(line.match(/(\/\/\s*TODO.*)/)[0]);
+                    }
+                }
+            }
         case 'exit':
             process.exit(0);
             break;
